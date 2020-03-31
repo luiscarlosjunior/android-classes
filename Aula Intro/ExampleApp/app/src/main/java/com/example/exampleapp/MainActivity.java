fd4863d7b8f298506e6cd46b7a40863b4dd22493
Luis Santos <luis.carlosjr@hotmail.com.br>
2020-03-31 09:39:45 -0300

    Add comments

--- Aula Intro/ExampleApp/app/src/main/java/com/example/exampleapp/MainActivity.java
+++ Aula Intro/ExampleApp/app/src/main/java/com/example/exampleapp/MainActivity.java
@@ -10,8 +10,11 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     public void clickFuncion(View view) {
+        // Instância e inicializa uma variável
         EditText editText = (EditText) findViewById(R.id.editTextView);
+        // Cria um log (saída de mensagem no console) para verificar se foi pressionado
         Log.i("Info","Button Pressed");
+        // Pega o texto e mostra no console
         Log.i("Values", editText.getText().toString());
     }
 
