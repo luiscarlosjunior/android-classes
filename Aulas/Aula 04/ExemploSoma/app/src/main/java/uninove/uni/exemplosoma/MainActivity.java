c53837736f4eba459244bb105c8b7d3584c21f98
luiscarlosjunior <luis.carlosjr@hotmail.com.br>
2020-05-08 17:18:19 -0300

    Fixed code

--- Aulas/Aula 04/ExemploSoma/app/src/main/java/uninove/uni/exemplosoma/MainActivity.java
+++ Aulas/Aula 04/ExemploSoma/app/src/main/java/uninove/uni/exemplosoma/MainActivity.java
@@ -1,10 +1,23 @@
 package uninove.uni.exemplosoma;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public void somaValores(View view) {
+
+        EditText valor1 = (EditText) findViewById(R.id.valor1);
+        EditText valor2 = (EditText) findViewById(R.id.valor2);
+
+        int soma = Integer.parseInt(valor1.getText().toString()) + Integer.parseInt(valor2.getText().toString());
+
+        Toast.makeText(this, "A soma é " + soma, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
